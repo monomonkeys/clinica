d0a1869070b862ee17c6cdc4fc8966be1e6b7581
Cunille, Joaquin <71965+212804587@users.noreply.github.build.ge.com>
2020-10-20 22:02:48 -0500

    comment

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginUsuario } from '../models/login-usuario';
+import { AuthService } from '../service/auth.service';
+import { TokenService } from '../service/token.service';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  isLogged = false;
+  isLoginFail = false;
+  loginUsuario: LoginUsuario;
+  nombreUsuario: string;
+  password: string;
+  roles: string[] = [];
+  errMsj: string;
+
+  constructor(
+    private tokenService: TokenService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+      this.isLoginFail = false;
+      this.roles = this.tokenService.getAuthorities();
+    }
+
     document.querySelector('.img-btn').addEventListener('click',function(){
       document.querySelector('.cont').classList.toggle('s-signup')
     });
+  }
+
+  onLogin(): void {
+    this.loginUsuario = new LoginUsuario(this.nombreUsuario, this.password);
+    this.authService.login(this.loginUsuario).subscribe(
+      data => {
+        this.isLogged = true;
+        this.isLoginFail = false;
+
+        this.tokenService.setToken(data.token);
+        this.tokenService.setUserName(data.nombreUsuario);
+        this.tokenService.setAuthorities(data.authorities);
+        this.roles = data.authorities;
+        /*this.toastr.success('Bienvenido ' + data.nombreUsuario, 'OK', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+        this.router.navigate(['/']);*/
+      },
+      err => {
+        this.isLogged = false;
+        this.isLoginFail = true;
+        this.errMsj = err.error.message;
+        /*this.toastr.error(this.errMsj, 'Fail', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });*/
+        console.log(this.errMsj);
+      }
+    );
   }
   
   
